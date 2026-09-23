3ef4af63911d0d727787d9d24d071487ee19627a
Zakii305 <78925093+Zakii305@users.noreply.github.com>
2026-09-23 20:25:20 +0500

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2640,11 +2640,11 @@ var cnames_active = {
   "pwa-workshop": "sylvainpolletvillard.github.io/pwa-workshop",
   "py-like": "ruben-arushanyan.github.io/py-like",
   "pytml": "nodex-ar.github.io/Pytml",
+  "qasmi": "qasmi-js.surge.sh",
   "qbit": "fudan-mse.github.io/qbit",
   "qredential": "george-veras.github.io/qredential",
   "qs": "kirjs.github.io/qs.js", // noCF? (don´t add this in a new PR)
   "quacky": "quacky-bot.github.io",
-  "qasmi": "qasmi-js.surge.sh",
   "quanta": "solarbrowser.github.io/w4q",
   "quantlib": "quantlibjs.github.io",
   "quantum": "militia21.github.io/quantum",
